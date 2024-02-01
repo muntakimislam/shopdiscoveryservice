dd4cf163f81be1870782b1f6d5dc999d6300dc78
Muntakim Islam Idrak <43186205+idrak07@users.noreply.github.com>
2024-02-01 22:28:04 +0600

    Init discovery service for online shopping service

---
+++ src/test/java/com/learnspring/cloud/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.cloud.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
